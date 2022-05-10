d8f5fbf88c1c2886ccad7a772ecc99037abfc03f
Sumeetacz <sumeetacz@gmail.com>
2022-05-10 23:12:40 +0530

    UC2 problem Peak And Pop from the stack till empty

---
+++ StackAndQueues/UC2_PickAndPop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_Queue
+{
+    public class Stacks
+    {
+        Linked_List linked_list;
+
+        public Stacks()
+        {
+            linked_list = new Linked_List();
+        }
+        public void Push(Node node)
+        {
+            linked_list.AddNode(node);
+        }
+        public void Display()
+        {
+            linked_list.Display();
+        }
+        public void Peek()
+        {
+            var Value = linked_list.Head.Data;
+            Console.WriteLine("The topmost value is " + Value);
+        }
+        public void Pop()
+        {
+            linked_list.Delete_First();
+        }
+    }
+}
